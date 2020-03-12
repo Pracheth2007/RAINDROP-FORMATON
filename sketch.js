2e27d584c29648ace149ca5be3bbfb64d6a22289
Pracheth2007 <60105879+Pracheth2007@users.noreply.github.com>
2020-03-12 10:46:37 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 10, 50);
+  createSprite(100,200,10,50);
+  createSprite(450,200,10,50);
+  createSprite(150,200,10,50);
+  createSprite(350,200,10,50);
+  createSprite(300,200,10,50);
+  createSprite(250,200,10,50);
+  createSprite(700,200,10,50);
+  createSprite(200,10,10,50);
+  createSprite(200,300,10,50);
+  createSprite(200,360,10,50);
+  createSprite(200,50,10,50);
+  createSprite(200,110,10,50);
+  createSprite(50,170,10,50);
+
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
